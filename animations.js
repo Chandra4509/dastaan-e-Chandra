f545ab4f867b04b6faba0affa2625da2caa3bd58
Chandra4509 <chandraprakashmohanty4@gmail.com>
2024-10-01 21:46:44 +0530

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,6 +1,4 @@
-// Simple example of a rotating animation on page load
-window.onload = function() {
-    const graphic = document.querySelector('.graphic-element');
-    graphic.style.transition = 'transform 1s ease-in-out';
-    graphic.style.transform = 'rotate(360deg)';
-};
+function toggleSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('show'); // Toggle 'show' class to control visibility
+}
